1d80e13781745b98460b278f6a5cff9dd91b5531
eddie Hinojosa <eddie_hinojosa@live.com>
2024-07-06 15:06:42 -0400

    added see files for final test-run

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,3 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize (process.env.POSTGRESURI)
 
 module.exports = sequelize;
-
-// require('dotenv').config();
-
-// const Sequelize = require('sequelize');
-
-// const sequelize = process.env.DB_URL
-//   ? new Sequelize(process.env.DB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//       host: 'localhost',
-//       dialect: 'postgres',
-//       dialectOptions: {
-//         decimalNumbers: true,
-//       },
-//     });
-
-// module.exports = sequelize;
